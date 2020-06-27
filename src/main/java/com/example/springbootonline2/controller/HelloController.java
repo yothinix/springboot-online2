bc1f50a2d03d5e194429823af3b59be7a98bcc55
Yothin Muangsommuk <m@yothinix.com>
2020-06-27 16:05:15 +0700

    Using RestController instead of Controller so that we don't need to add ResponseBody every time

--- src/main/java/com/example/springbootonline2/controller/HelloController.java
+++ src/main/java/com/example/springbootonline2/controller/HelloController.java
@@ -1,17 +1,14 @@
 package com.example.springbootonline2.controller;
 
 import com.example.springbootonline2.model.MessageInfo;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
     @RequestMapping("/hello")
-    @ResponseBody
     public MessageInfo sayHello() {
-//        return "Hello world, welcome to spring boot.";
         MessageInfo messageInfo = new MessageInfo();
         messageInfo.setMessage("Hello world, welcome to spring boot.");
 
