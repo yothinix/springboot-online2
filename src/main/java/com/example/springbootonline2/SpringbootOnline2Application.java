11fa288cdc88b2fccd64924c0143b70c5c0f50f0
Yothin Muangsommuk <m@yothinix.com>
2020-06-27 15:15:09 +0700

    :tada: Initial project :rocket:

---
+++ src/main/java/com/example/springbootonline2/SpringbootOnline2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootonline2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOnline2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOnline2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootonline2/SpringbootOnline2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootonline2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOnline2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
