7cee52ffb27d1cca9da781e8b2f87f920444a815
Yothin Muangsommuk <m@yothinix.com>
2020-06-27 16:10:12 +0700

    Return List message instead :rocket:

--- src/main/java/com/example/springbootonline2/controller/HelloController.java
+++ src/main/java/com/example/springbootonline2/controller/HelloController.java
@@ -4,14 +4,19 @@ import com.example.springbootonline2.model.MessageInfo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class HelloController {
 
     @RequestMapping("/hello")
-    public MessageInfo sayHello() {
+    public List<MessageInfo> sayHello() {
         MessageInfo messageInfo = new MessageInfo();
         messageInfo.setMessage("Hello world, welcome to spring boot.");
 
-        return messageInfo;
+        List<MessageInfo> messageInfoList = new ArrayList<>();
+        messageInfoList.add(messageInfo);
+        return messageInfoList;
     }
 }
