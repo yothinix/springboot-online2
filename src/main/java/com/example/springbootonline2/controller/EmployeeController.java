67d9775d5014a102660c9edf5546ed3af47a5d7b
Yothin Muangsommuk <m@yothinix.com>
2020-06-28 11:50:23 +0700

    Define Request mapping on class level, So that we don't need to write /employees everytime :rocket:

--- src/main/java/com/example/springbootonline2/controller/EmployeeController.java
+++ src/main/java/com/example/springbootonline2/controller/EmployeeController.java
@@ -8,28 +8,28 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/employees")
 public class EmployeeController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @RequestMapping(value = "/employees", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public List<Employee> listAllEmployee() {
         return employeeRepository.listAll();
     }
 
-    @RequestMapping(value = "/employees/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Employee getEmployee(@PathVariable Integer id) {
         return employeeRepository.findById(id);
     }
 
-    @RequestMapping(value = "/employees", method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     public void createEmployee(@RequestBody Employee employee) {
         employeeRepository.save(employee);
     }
 
-    @RequestMapping(value = "/employees/{id}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public void updateEmployee(@PathVariable Integer id, @RequestBody Employee employee) {
-
     }
 }
